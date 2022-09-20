ba6031c69fcf50bf9caeb53060646fb7c28a8066
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2022-09-20 11:14:01 +0200

    Updated help

--- cmd/root.go
+++ cmd/root.go
@@ -29,13 +29,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "bammask",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Mask bases in input bam file",
+	Long: `This tool just reads a bam file and replace bases that are below a given quality threshold by N.
+	`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
